620d8d15023be85c2ed279357c1dcc24764dd6ce
jeonghan-lee <qlqhqo2341@naver.com>
2021-05-16 00:38:44 +0900

    타이포 그래피 샘플 추가

--- src/component/content/fragment/HomeFragment.tsx
+++ src/component/content/fragment/HomeFragment.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Typography } from 'antd'
+
+const { Paragraph, Title, Link } = Typography
 
 function Home () {
-  return (
-      <>
-        <h3>
-            <a href="https://home.jeonghan.wo.tc">home 링크</a>
-        </h3>
-      </>
-  )
+  return <h3>
+      <a href="https://home.jeonghan.wo.tc">home 링크</a>
+      <Typography>
+        <Title> 타이포 그래피 제목</Title>
+        <Paragraph>
+          로렘 입숨~~~ <Link href="https://www.naver.com">네이버링크</Link>
+        </Paragraph>
+      </Typography>
+  </h3>
 }
 
 export default Home
